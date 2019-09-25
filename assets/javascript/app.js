18491480ee3d66730ab72ddd64734c31f4de26e6
Angie Roach <angieroach4@gmail.com>
2019-09-25 08:23:23 -0400

    coded homepage, all but modal

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,23 @@
+var slideIndex = 0;
+showSlides();
 
+function showSlides() {
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (var i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (var i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 3000); // Change image every 3 seconds
+}
 
-
- var macys = ["Fall Sweaters", "Fall Jackets", "Fall Shirts","Fall Pants", "Fall Dresses"];
- // var queryURL = "https://www.omdbapi.com/?t=" + title + "&y=&plot=short&apikey=trilogy";
- 
- var clothes = $(this).attr("data-name");
- var queryURL = "http://api.macys.com/v4/catalog/search?searchphrase=fall+dresses&x-macys-webservice-client-id:pds2kqageazpfkmtmhzk3qbm"
-
- $.ajax({
-   url: queryURL,
-   headers:{
-    Accept: "application/json", // you can quote the key here too, but the value has to be quoted.
-    "x-macys-webservice-client-id:pds2kqageazpfkmtmhzk3qbm" // the key must be quoted since - aren't allowed in key names. if apikey is not a variable, then it should be quoted too.
-},
-   method: "GET"
- }).then(function(response) {                //can call it by function(res) or function(data) but we will use function(response)
-   console.log(response);                    // can call specific elements of JSON
-     
- });
-
-
- //http://developer.macys.com/io-docs
- //http://developer.macys.com/docs/catalog_and_store_services/catalog/search/#queryparameters
+$(".hamburger").click(function(){
+    $("#nav").toggle();   
+       
+   });     
